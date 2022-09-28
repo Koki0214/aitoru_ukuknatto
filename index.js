fb9c940187be78c855e6ec412ec10493bcafa20e
Koki3110 <114120002+Koki0214@users.noreply.github.com>
2022-09-28 07:34:29 +0000

    ボタンクリック時にDB変更
    予約するというボタンをクリックされると、DBの内容を変更。
    
    #3

--- index.js
+++ index.js
@@ -19,11 +19,35 @@ const exists = fs.existsSync(dbfile);
 const db = new sqlite3.Database(dbfile);
 
 //起動時に予約画面を表示する
-app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'public/yoyaku.html')))
+//app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'public/yoyaku.html')))
 //urlでreservecheckが指定された時、予約確認画面に遷移する
-app.get('/reservecheck',(req,res)=>res.sendFile(path.join(__dirname,'public/reservecheck.html')))
+//app.get('/reservecheck',(req,res)=>res.sendFile(path.join(__dirname,'public/reservecheck.html')))
 //予約確認画面で、予約画面に戻るボタンを押された時、予約画面に遷移する
-app.get('/yoyaku',(req,res)=>res.sendFile(path.join(__dirname,'public/yoyaku.html')))
+//app.get('/yoyaku',(req,res)=>res.sendFile(path.join(__dirname,'public/yoyaku.html')))
+
+app.get('/',(req,res) => {
+  const query = "select id,name from book;";
+  db.all(query,(err,rows)=>{
+    if(err){
+      console.log(err.message);
+    }
+    res.render("yoyaku.ejs",{results:rows});
+  })
+});
+
+app.get('/reservecheck/:id',(req,res)=> {
+  const id=req.params.id;
+  const query = "select name from book where id=?";
+  
+  db.all(query,[id],(err,rows)=>{
+    if(err){
+      console.log(err.message);
+    }
+    res.render("reservecheck.ejs",{results:rows});
+  })
+});
+
+
 
 app.listen(port, () => 
   console.log('server started'));
