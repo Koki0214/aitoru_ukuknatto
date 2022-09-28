8c6894c15fcfd02f37d76fb6ae79171c20a5becc
Koki3110 <114120002+Koki0214@users.noreply.github.com>
2022-09-28 06:51:43 +0000

    予約確認画面から予約画面遷移
    予約確認画面にある「予約画面に戻る」をクリックし、予約画面に戻る。
    
    #3

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ const app = express()
 const path = require('path');
 const port = 3000
 
+const bodyParser = require("body-parser");
+app.set("view_engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+module.exports=app;
 
+const sqlite3 = require('sqlite3').verbose();
+const fs = require("fs");
+const dbfile="room.db";
+const exists = fs.existsSync(dbfile);
+const db = new sqlite3.Database(dbfile);
+
+//起動時に予約画面を表示する
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'public/yoyaku.html')))
-//app.get('/',function(req,res){
-  //console.log(req);
-  //res.send('<h3>Hello World!</h3><h1>アイトル</h1><h2>-技大用教室予約システム-</h2>');
-//});
+//urlでreservecheckが指定された時、予約確認画面に遷移する
 app.get('/reservecheck',(req,res)=>res.sendFile(path.join(__dirname,'public/reservecheck.html')))
+//予約確認が
+app.get('/yoyaku',(req,res)=>res.sendFile(path.join(__dirname,'public/yoyaku.html')))
+
 app.listen(port, () => 
   console.log('server started'));
