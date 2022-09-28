cde4037648d616405e7ae94824b624e51bdd6812
Shiyu <110214631+Shiyukari@users.noreply.github.com>
2022-09-28 05:25:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//varstemName = document.getElementById('main'); 
+//var teamName = document.getElementById('product');
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000
+
+
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'public/yoyaku.html')))
+//app.get('/',function(req,res){
+  //console.log(req);
+  //res.send('<h3>Hello World!</h3><h1>アイトル</h1><h2>-技大用教室予約システム-</h2>');
+//});
+app.listen(port, () => 
+  console.log('server started'));
